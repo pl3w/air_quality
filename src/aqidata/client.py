5159772d75d345b0a39372fad7af1a0d389dd514
pl3w <s110810531@student.nqu.edu.tw>
2022-09-27 13:24:59 +0800

    pwd

--- src/aqidata/client.py
+++ src/aqidata/client.py
@@ -3,8 +3,8 @@ from sqlalchemy import create_engine, engine
 def get_airquality_conn():
 
     global engine
-    address = "mysql+pymysql://pl3w:Pbw101031@mysql:3306/aqidb"
-    #address = "mysql+pymysql://pl3w:Pbw101031@139.162.119.7:3306/aqidb"
+    # MySQL Address
+    address = "" 
 
     engine = create_engine(address)
     return engine.connect()
